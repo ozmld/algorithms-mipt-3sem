b0ef4cb081c88e7c14bba2e54b3fa79ecef43e30
ozmld <57392266+ozmld@users.noreply.github.com>
2023-10-16 03:36:19 +0300

    Update D.cpp

--- contest-10/D.cpp
+++ contest-10/D.cpp
@@ -9,18 +9,11 @@
 #include <queue>
 #include <set>
 
-template <typename T>
-void PrintVector(std::vector<T>& vec) {
-  for (size_t i = 0; i < vec.size(); ++i) {
-    std::cout << vec[i] << " ";
-  }
-  std::cout << "\n";
-}
-
 const size_t kAlphSize = 26;
 const int kAlphabet[kAlphSize] = {0,  1,  2,  3,  4,  5,  6,  7,  8,
                                   9,  10, 11, 12, 13, 14, 15, 16, 17,
                                   18, 19, 20, 21, 22, 23, 24, 25};
+
 std::vector<std::vector<int>> possible_match;
 int threshold = 0;
 int match_num = 0;
@@ -28,7 +21,6 @@ int match_num = 0;
 struct Node {
   int to[kAlphSize];
   bool term;
-  int sufflink;
   std::vector<int> term_ind;
   int link;
   int go[kAlphSize];
@@ -41,45 +33,47 @@ struct Node {
     cnt = 0;
   }
 };
-struct Trie {
-  std::vector<Node> bor;
-  size_t patterns_num = 0;
-  void Add(std::string& str, int ind) {
-    ++patterns_num;
-    if (bor.empty()) {
-      bor.push_back(Node());
-    }
-    int vertex = 0;
-    for (size_t i = 0; i < str.length(); ++i) {
-      if (bor[vertex].to[str[i] - 'a'] == -1) {
-        bor[vertex].to[str[i] - 'a'] = bor.size();
-        bor.push_back(Node());
-      }
-      vertex = bor[vertex].to[str[i] - 'a'];
-    }
-    bor[vertex].term = true;
-    bor[vertex].term_ind.push_back(ind);
-  }
+class Trie {
+  std::vector<Node> trie_;
+  size_t patterns_num_ = 0;
   void CntIndexesUpdate(int vertex_v) {
-    if (bor[vertex_v].term) {
-      for (auto elem : bor[vertex_v].term_ind) {
-        bor[vertex_v].cnt_indexes.push_back(elem);
+    if (trie_[vertex_v].term) {
+      for (auto elem : trie_[vertex_v].term_ind) {
+        trie_[vertex_v].cnt_indexes.push_back(elem);
       }
     }
-    if (bor[vertex_v].link != -1) {
-      for (auto elem : bor[bor[vertex_v].link].cnt_indexes) {
-        bor[vertex_v].cnt_indexes.push_back(elem);
+    if (trie_[vertex_v].link != -1) {
+      for (auto elem : trie_[trie_[vertex_v].link].cnt_indexes) {
+        trie_[vertex_v].cnt_indexes.push_back(elem);
       }
     }
   }
 
-  void Build() {
-    bor[0].link = -1;
+ public:
+  Trie() { trie_ = {Node()}; }
+  void AddString(std::string& str, int ind) {
+    ++patterns_num_;
+    if (trie_.empty()) {
+      Trie();
+    }
+    int vertex = 0;
+    for (size_t i = 0; i < str.length(); ++i) {
+      if (trie_[vertex].to[str[i] - 'a'] == -1) {
+        trie_[vertex].to[str[i] - 'a'] = trie_.size();
+        trie_.push_back(Node());
+      }
+      vertex = trie_[vertex].to[str[i] - 'a'];
+    }
+    trie_[vertex].term = true;
+    trie_[vertex].term_ind.push_back(ind);
+  }
+  void BuildTrie() {
+    trie_[0].link = -1;
     for (auto character : kAlphabet) {
-      if (bor[0].to[character] != -1) {
-        bor[0].go[character] = bor[0].to[character];
+      if (trie_[0].to[character] != -1) {
+        trie_[0].go[character] = trie_[0].to[character];
       } else {
-        bor[0].go[character] = 0;
+        trie_[0].go[character] = 0;
       }
     }
     std::queue<int> vertices_queue;
@@ -88,37 +82,37 @@ struct Trie {
       int vertex_v = vertices_queue.front();
       vertices_queue.pop();
       CntIndexesUpdate(vertex_v);
-      bor[vertex_v].cnt =
-          (bor[vertex_v].term ? 1 : 0) +
-          (bor[vertex_v].link == -1 ? 0 : bor[bor[vertex_v].link].cnt);
+      trie_[vertex_v].cnt =
+          (trie_[vertex_v].term ? 1 : 0) +
+          (trie_[vertex_v].link == -1 ? 0 : trie_[trie_[vertex_v].link].cnt);
       for (auto character1 : kAlphabet) {
-        int vertex_u = bor[vertex_v].to[character1];
+        int vertex_u = trie_[vertex_v].to[character1];
         if (vertex_u == -1) {
           continue;
         }
-        bor[vertex_u].link =
-            (vertex_v == 0 ? 0 : bor[bor[vertex_v].link].go[character1]);
+        trie_[vertex_u].link =
+            (vertex_v == 0 ? 0 : trie_[trie_[vertex_v].link].go[character1]);
         for (auto character2 : kAlphabet) {
-          if (bor[vertex_u].to[character2] != -1) {
-            bor[vertex_u].go[character2] = bor[vertex_u].to[character2];
+          if (trie_[vertex_u].to[character2] != -1) {
+            trie_[vertex_u].go[character2] = trie_[vertex_u].to[character2];
           } else {
-            bor[vertex_u].go[character2] =
-                bor[bor[vertex_u].link].go[character2];
+            trie_[vertex_u].go[character2] =
+                trie_[trie_[vertex_u].link].go[character2];
           }
         }
         vertices_queue.push(vertex_u);
       }
     }
   }
-
-  void FindAllMatches(std::string& text, size_t pos, size_t treshold,
-                      size_t shift, std::string& type) {
+  int GetNext(int vertex, char symb) { return trie_[vertex].go[symb - 'a']; }
+  void FindAllMatches(std::string& text, size_t pos, size_t bound, size_t shift,
+                      std::string& type) {
     int vertex = 0;
     for (size_t i = 0; i < text.length(); ++i) {
-      vertex = bor[vertex].go[text[i] - 'a'];
-      for (auto ind : bor[vertex].cnt_indexes) {
-        if (pos + patterns_num > treshold + ind ||
-            ind >= std::min(pos + 1, patterns_num)) {
+      vertex = trie_[vertex].go[text[i] - 'a'];
+      for (auto ind : trie_[vertex].cnt_indexes) {
+        if (pos + patterns_num_ > bound + ind ||
+            ind >= std::min(pos + 1, patterns_num_)) {
           continue;
         }
         if (type == "rows") {
@@ -138,33 +132,25 @@ struct Trie {
   }
 };
 
-size_t CountMatchesNum(std::vector<std::vector<int>>& possible_match,
-                       size_t treshold) {
-  size_t match_num = 0;
-  for (size_t i = 0; i < possible_match.size(); ++i) {
-    for (size_t elem : possible_match[i]) {
-      if (elem + 2 >= treshold) {
-        ++match_num;
-      }
-    }
+void CountMapMatches(std::vector<std::string>& moscow_rows,
+                     std::vector<std::string>& moscow_columns,
+                     std::vector<std::string>& mipt_rows,
+                     std::vector<std::string>& mipt_columns) {
+  threshold = mipt_rows.size() + mipt_columns.size() - 2;
+  if (threshold == 0) {
+    match_num = moscow_rows.size() * moscow_columns.size();
+    return;
   }
-  return match_num;
-}
-
-void Katya(std::vector<std::string>& moscow_rows,
-           std::vector<std::string>& moscow_columns,
-           std::vector<std::string>& mipt_rows,
-           std::vector<std::string>& mipt_columns) {
   Trie trie_rows;
   Trie trie_columns;
   for (size_t i = 0; i < mipt_rows.size(); ++i) {
-    trie_rows.Add(mipt_rows[i], i);
+    trie_rows.AddString(mipt_rows[i], i);
   }
   for (size_t i = 0; i < mipt_columns.size(); ++i) {
-    trie_columns.Add(mipt_columns[i], i);
+    trie_columns.AddString(mipt_columns[i], i);
   }
-  trie_rows.Build();
-  trie_columns.Build();
+  trie_rows.BuildTrie();
+  trie_columns.BuildTrie();
   possible_match.resize(moscow_rows.size(),
                         std::vector<int>(moscow_columns.size(), 0));
   std::string type = "rows";
@@ -200,11 +186,7 @@ int main() {
   size_t mipt_map_height;
   size_t mipt_map_width;
   std::cin >> mipt_map_height >> mipt_map_width;
-  threshold = mipt_map_height + mipt_map_width - 2;
-  if (threshold == 0) {
-    std::cout << moscow_map_height * moscow_map_width;
-    return 0;
-  }
+
   std::vector<std::string> mipt_rows(mipt_map_height);
   std::vector<std::string> mipt_columns(mipt_map_width);
   for (size_t i = 0; i < mipt_map_height; ++i) {
@@ -215,6 +197,6 @@ int main() {
       mipt_columns[jj] += row[jj];
     }
   }
-  Katya(moscow_rows, moscow_columns, mipt_rows, mipt_columns);
+  CountMapMatches(moscow_rows, moscow_columns, mipt_rows, mipt_columns);
   std::cout << match_num;
 }
